3847f2b626ebe4012f7c5b062b9631ae82d4c077
22bipp22 <tammibippert@gmail.com>
2020-09-03 10:28:47 -0500

    Completed creating charts in python

--- EmployeeSQL/tables.sql
+++ EmployeeSQL/tables.sql
@@ -5,6 +5,12 @@ DROP TABLE dept_manager;
 DROP TABLE titles;
 DROP TABLE salaries;
 
+CREATE TABLE titles (
+	title_id VARCHAR(10),
+	title VARCHAR(30),
+	PRIMARY KEY (title_id)	
+);
+
 CREATE TABLE employees (
 	emp_no INT,
 	emp_title VARCHAR(10),
@@ -13,7 +19,8 @@ CREATE TABLE employees (
 	last_name VARCHAR(30),
 	sex VARCHAR(1),
 	hire_date DATE,
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY(emp_title) REFERENCES titles (title_id)
 );
 
 CREATE TABLE departments (
@@ -24,22 +31,28 @@ CREATE TABLE departments (
 
 CREATE TABLE dept_emp (
 	emp_no INT,
-	dept_no VARCHAR(10) 
-	
+	dept_no VARCHAR(10),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY(emp_no, dept_no)
 );
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(10),
-	emp_no INT
+	emp_no INT,
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY(emp_no, dept_no)
 );
 
-CREATE TABLE titles (
-	title_id VARCHAR(10),
-	title VARCHAR(30),
-	PRIMARY KEY (title_id)
-);
 
 CREATE TABLE salaries (
 	emp_no INT,
-	salary INT
+	salary INT,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
 );
+
+
+
+
+
